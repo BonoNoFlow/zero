6cb39f94366387796e9bc1c7d9e0c2fc3be6ce54
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-05-15 04:09:18 +0200

    new concept!!!!

--- src/main/java/com/bono/controls/CurrentPlaylist.java
+++ src/main/java/com/bono/controls/CurrentPlaylist.java
@@ -11,7 +11,7 @@ import java.util.Iterator;
 /**
  * Created by hendriknieuwenhuis on 26/04/16.
  */
-public class CurrentPlaylist extends MouseAdapter implements ListDataListener {
+public class CurrentPlaylist {
 
     private PlaylistView playlistView;
 
@@ -23,28 +23,12 @@ public class CurrentPlaylist extends MouseAdapter implements ListDataListener {
 
     public CurrentPlaylist(PlaylistControl playlistControl) {
         this.playlistControl = playlistControl;
-        songs.addListDataListener(this);
-        //initPlaylist();
     }
-
-    /*
-    public CurrentPlaylist(DBExecutor dbExecutor, boolean initPlaylist) {
-        this(dbExecutor);
-        if (initPlaylist) initPlaylist();
-    }
-
-    public CurrentPlaylist(DBExecutor dbExecutor, PlaylistView playlistView, boolean initPlaylist) {
-        this(dbExecutor);
-        this.playlistView = playlistView;
-        if (initPlaylist) initPlaylist();
-    }*/
-
 
     // TODO dit kan zonder pPlaylist class!!!!
     public void initPlaylist() {
         playlist = new Playlist();
         String value = "";
-        songs.clear();
         try {
             playlist.populate(playlistControl.playlistinfo(null));
         } catch (ACKException ack) {
@@ -52,40 +36,18 @@ public class CurrentPlaylist extends MouseAdapter implements ListDataListener {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        Iterator<Song> i = playlist.iterator();
-        while (i.hasNext()) {
-            songs.addElement(i.next());
+
+        songs.clear();
+        for (Song s : playlist.playlist()) {
+            songs.addElement(s);
         }
-        System.out.println("donr!");
+
         if (playlistView != null) {
             SwingUtilities.invokeLater(() -> {
                 playlistView.getPlaylist().setModel(songs);
             });
         }
-
      }
-
-    @Override
-    public void intervalAdded(ListDataEvent e) {
-        System.out.println("added");
-    }
-
-    @Override
-    public void intervalRemoved(ListDataEvent e) {
-        System.out.println("removed");
-    }
-
-    @Override
-    public void contentsChanged(ListDataEvent e) {
-        System.out.println("Data list changed");
-        DefaultListModel<Song> list = (DefaultListModel<Song>) e.getSource();
-
-        if (playlistView != null) {
-            SwingUtilities.invokeLater(() -> {
-                playlistView.getPlaylist().setModel(list);
-            });
-        }
-    }
 
     public PlaylistView getPlaylistView() {
         return playlistView;
