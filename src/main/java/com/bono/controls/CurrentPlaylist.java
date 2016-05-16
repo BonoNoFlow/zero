16cd3e71ae481017e992648f39d9385d346e2b03
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-05-17 01:47:40 +0200

    new concept!!!!

--- src/main/java/com/bono/controls/CurrentPlaylist.java
+++ src/main/java/com/bono/controls/CurrentPlaylist.java
@@ -3,9 +3,6 @@ package com.bono.controls;
 import com.bono.api.*;
 import com.bono.view.PlaylistView;
 import javax.swing.*;
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
-import java.awt.event.MouseAdapter;
 import java.util.Iterator;
 
 /**
@@ -27,32 +24,20 @@ public class CurrentPlaylist {
 
     // TODO dit kan zonder pPlaylist class!!!!
     public void initPlaylist() {
-       // playlist = new Playlist();
+        playlist = new Playlist();
         String value = "";
         try {
-            //playlist.populate(playlistControl.playlistinfo(null));
-            value = playlistControl.playlistinfo(null);
+            playlist.populate(playlistControl.playlistinfo(null));
         } catch (ACKException ack) {
             ack.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        //System.out.println(value);
 
         songs.clear();
-        /*
         for (Song s : playlist.playlist()) {
             songs.addElement(s);
-        }*/
-        Reply reply = new Reply(value);
-        System.out.println(reply.toString());
-        Iterator<String> i = reply.iterator();
-        while (i.hasNext()) {
-            Song song = new Song(i.next());
-            System.out.println(song.toString());
-            //songs.addElement(new Song(i.next()));
         }
-
         if (playlistView != null) {
             SwingUtilities.invokeLater(() -> {
                 playlistView.getPlaylist().setModel(songs);
