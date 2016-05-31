3aad9c6d0e4dc909dbbf05f500b05f68713f84f5
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-05-31 04:23:07 +0200

    new concept!!!!

--- src/main/java/com/bono/config/ZeroConfig.java
+++ src/main/java/com/bono/config/ZeroConfig.java
@@ -6,17 +6,25 @@ import com.bono.api.Config;
  * Created by hendriknieuwenhuis on 31/05/16.
  *
  *  TODO controller / logic voor config!!!
+ *
+ *  properties populate met Strings.
+ *
+ *  add listeners en trigger ze als property gezet wordt.
  */
 public class ZeroConfig extends Config {
 
-
+    private final String HOST_PROPERTY = "host_property";
+    private final String PORT_PROPERTY = "port_property";
+    private final String SOUNDCLOUD_RESULTS = "soundcloud_results";
 
     public ZeroConfig() {
         super();
     }
 
     public ZeroConfig(String host, int port) {
-        super(host, port);
+        //super(host, port);
+        properties.setProperty(HOST_PROPERTY, host);
+        properties.setProperty(PORT_PROPERTY, Integer.toString(port));
     }
 
     public int getSoundcloudResults() {
