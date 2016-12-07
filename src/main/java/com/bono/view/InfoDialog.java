d3709e111d4348af659fce240dc5fea28a2a1634
hendriknieuwenhuis <hendrik.nieuwenhuis@gmail.com>
2016-12-07 03:25:55 +0100

    added infodialog mock up

---
+++ src/main/java/com/bono/view/InfoDialog.java
@@ -0,0 +1,13 @@
+package com.bono.view;
+
+import javax.swing.*;
+
+/**
+ * Created by hendriknieuwenhuis on 07/12/2016.
+ */
+public class InfoDialog extends JDialog {
+
+    public InfoDialog() {
+        super();
+    }
+}
